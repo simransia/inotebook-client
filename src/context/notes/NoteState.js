4dd20175f6aa8d03ceba3546e00af8821c3daece
Simran <88017902+simransia@users.noreply.github.com>
2022-12-10 21:50:40 +0530

    Update NoteState.js
    changed url

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -9,7 +9,7 @@ const NoteState = (props) => {
 
   //get all notes
   const getNote = async () => {
-    const response = await fetch('https://inotebook-server.onrender.com/api/notes/fetchallnotes', {
+    const response = await fetch('https://inotebook-server.up.railway.app/api/notes/fetchallnotes', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -25,7 +25,7 @@ const NoteState = (props) => {
   //add a note
   const addNote = async (title, description, tag) => {
 
-    const response = await fetch('https://inotebook-server.onrender.com/api/notes/addnote', {
+    const response = await fetch('https://inotebook-server.up.railway.app/api/notes/addnote', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -47,7 +47,7 @@ const NoteState = (props) => {
   //delete a note
   const deleteNote = async(id) => {
     //a\Api call to delete
-    const response = await fetch(`https://inotebook-server.onrender.com/api/notes/deletenote/${id}`, {
+    const response = await fetch(`https://inotebook-server.up.railway.app/api/notes/deletenote/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -69,7 +69,7 @@ const NoteState = (props) => {
   //edit a note
   const editNote = async (id, title, description, tag) => {
 
-    const response = await fetch(`https://inotebook-server.onrender.com/api/notes/updatenote/${id}`, {
+    const response = await fetch(`https://inotebook-server.up.railway.app/api/notes/updatenote/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
